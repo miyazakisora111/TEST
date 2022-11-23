241162f4a5ae41c976cffa533ee8a77e9e16ed4f
miyazakisora111 <118680697+miyazakisora111@users.noreply.github.com>
2022-11-24 00:33:02 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict"
+{
+  const open = document.getElementById("open");
+  const slide = document.getElementById("slide");
+
+  open.addEventListener("click", () => {
+      slide.classList.add(".appear");
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
